d30c73614a141abadf7d5d6bb8af4d554ab43b02
efarhan <elias.farhan@gmail.com>
2019-04-03 10:21:24 +0200

    Adding MSVC optimized CPU flags

--- include/graphics.h
+++ include/graphics.h
@@ -19,10 +19,10 @@ public:
 	void SetVec3(const std::string& name, float x, float y, float z) const;
 	void SetVec3(const std::string &name, const glm::vec3 &value) const;
 	void SetVec4(const std::string& name, float x, float y, float z, float w);
-	void  Shader::SetVec4(const std::string &name, const glm::vec4 &value) const;
-	void  Shader::SetMat2(const std::string &name, const glm::mat2 &mat) const;
-	void  Shader::SetMat3(const std::string &name, const glm::mat3 &mat) const;
-	void  Shader::SetMat4(const std::string &name, const glm::mat4 &mat) const;
+	void SetVec4(const std::string &name, const glm::vec4 &value) const;
+	void SetMat2(const std::string &name, const glm::mat2 &mat) const;
+	void SetMat3(const std::string &name, const glm::mat3 &mat) const;
+	void SetMat4(const std::string &name, const glm::mat4 &mat) const;
 private:
 	int shaderProgram = 0;
 };
